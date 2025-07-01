9e3a20ffc6fbf4ec5b4b70329d73b631dbc1db60
SuditJha <jsudit2@gmail.com>
2025-07-01 18:34:55 +0530

    BeforeClass & AfterClass

--- src/test/java/com/in28minutes/junit/helper/QuickBeforeAfterTest.java
+++ src/test/java/com/in28minutes/junit/helper/QuickBeforeAfterTest.java
@@ -3,16 +3,27 @@ package com.in28minutes.junit.helper;
 import static org.junit.Assert.*;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class QuickBeforeAfterTest {
+	
+	@BeforeClass
+	public static void setupBeforeClass() {
+		System.out.println("In before Class");
+		System.out.println("It is Static method ");
+		System.out.println("Runs Once Before class execution starts");
+		System.out.println("==========================================");
+	}
 
 	@Before
 	public void setupBeforeTests() {
 		System.out.println("In Before Test");
 		System.out.println("Occurs before every Test");
 		System.out.println("Used to do some setup before running tests");
+//		System.out.println("==========================================");
 	}
 	
 	
@@ -33,7 +44,16 @@ public class QuickBeforeAfterTest {
 		System.out.println("Occurs after every test");
 		System.out.println("Tear Down Setup (:");
 		System.out.println("Write clean up code, connection close, etc");
-			
+		System.out.println("==========================================");
+	}
+	
+	@AfterClass
+	public static void tearDownAfterClass() {
+		System.out.println("In After Class");
+		System.out.println("It is Static method ");
+		System.out.println("Runs Once After Class ");
+		System.out.println("USed to maybe close db connection");
+	System.out.println("==========================================");
 	}
 	
 	
